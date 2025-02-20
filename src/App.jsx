424befec22a11bf3987010d4208025c1e73b9d00
dhanashri-musale20 <dhanashrimusale1@gmail.com>
2025-02-20 11:50:13 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ const Portfolio = () => {
 
       <footer className="bg-white py-6 mt-12">
         <div className="container mx-auto px-4 text-center">
-          <p className="text-black font-bold">©2025 All rights reserved</p>
+          <p className="text-black font-bold">©2020 All rights reserved</p>
         </div>
       </footer>
     </div>
